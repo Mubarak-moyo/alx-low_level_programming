e1e5b615614cf19e993550f192ff5bc68e63b7db
Mubarak <yolkadeolu@gmail.com>
2023-10-05 18:23:05 -0700

    maybe siuu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main -  Starting point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    iputs("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
