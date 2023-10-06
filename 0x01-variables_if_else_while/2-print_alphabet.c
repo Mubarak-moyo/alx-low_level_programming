8a5a34ad44db161e2771e64dbcdd0d5a247c1d78
Mubarak <yolkadeolu@gmail.com>
2023-10-06 12:40:25 -0700

    yh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+/**
+ *main - starting point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	    char letter;
+	char letter;
 
-	        for (letter = 'a'; letter <= 'z'; letter++)
-			    {
-				            putchar(letter);
-					        }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
 
-		    putchar('\n');
+	putchar('\n');
 
-		        return (0);
+	return (0);
 }
 
