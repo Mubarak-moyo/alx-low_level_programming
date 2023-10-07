7f80d1f100c7238a8b7a2677160bceee79685d4d
Mubarak <yolkadeolu@gmail.com>
2023-10-07 07:48:00 -0700

    tt

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - Starting code
+ * main - Starting main code
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
 	int i;
+	
 	for (i = 0; i < 10; i++)
 	{
-	printf("%d\n", i);
+	printf("%d", i);
 	}
 	return (0);
 }
