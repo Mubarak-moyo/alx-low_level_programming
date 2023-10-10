3abf366357064b8c2932d2fc7a3417f6739aeb6d
Mubarak <yolkadeolu@gmail.com>
2023-10-10 13:08:17 -0700

    test3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
+/**
+ * main - Starting point
+ *
+ * Return: 0(Success)
+ */
 int main(void)
 {
 	char str[] = "_putchar\n";
 	int i;
-	
-	for (i = 0; str[i] != '\0'; i++) {
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
 	}
-	
 	return (0);
 }
