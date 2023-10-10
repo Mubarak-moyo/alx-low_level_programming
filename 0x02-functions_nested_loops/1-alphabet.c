45e3ab3be9f180812d15a2a1ba1383051c9d56f5
Mubarak <yolkadeolu@gmail.com>
2023-10-10 14:13:51 -0700

    run

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,8 @@
 /**
  * Print_alphabet - prints the entire alphabet in lowercase followed by a new line
  */
+int main(void)
+{
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -12,11 +14,6 @@ void print_alphabet(void)
 		letter++;
 	}
 	
-	putchar('\n');
+	_putchar('\n');
 }
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
