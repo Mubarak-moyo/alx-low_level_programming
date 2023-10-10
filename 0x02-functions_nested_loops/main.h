2badf95065ed4ab26f719860e155c58d824f2b48
Mubarak <yolkadeolu@gmail.com>
2023-10-10 16:53:09 -0700

    pls

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,7 @@
 int _putchar(char);
 /* function for print alphabet */
 void print_alphabet(void);
+/* function that prints 10 times the alphabet, in lowercase, followed by a new line. */
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
