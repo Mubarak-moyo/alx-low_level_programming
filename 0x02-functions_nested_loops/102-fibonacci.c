e760836c8860795ae011de92a717a6fc9797573b
Mubarak <yolkadeolu@gmail.com>
2023-10-11 04:23:02 -0700

    try

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ fib[0] = 1;
 fib[1] = 2;
 
 int i;
-for (int i = 2; i < n; i++)
+for (i = 2; i < n; i++)
 {
 	fib[i] = fib[i - 1] + fib[i - 2];
 }
 
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
-	printf("%lld", fib[i]);
+	printf("%ld", fib[i]);
 	if (i < n - 1)
 	{
 		printf(", ");
