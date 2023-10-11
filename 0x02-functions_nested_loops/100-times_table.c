d51926f9182c300f67fbf8bcb8ff45cc3091277d
Mubarak <yolkadeolu@gmail.com>
2023-10-11 04:09:26 -0700

    move

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,14 +6,14 @@
  */
 void print_times_table(int n)
 {
+	int i;
+	int j;
+	int product;
+
 	if (n < 0 || n < 15)
 	{
 	return;
 	}
-
-	int i;
-	int j;
-	int product;
 
 	for (i = 0; i <= n; i++)
 	{
@@ -48,4 +48,5 @@ void print_times_table(int n)
 	}
 	_putchar('\n');
 	}
+
 }
