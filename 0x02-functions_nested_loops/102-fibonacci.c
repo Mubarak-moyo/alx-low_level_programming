dad726557628244ef6588d4bc0fa19d106659d51
Mubarak <yolkadeolu@gmail.com>
2023-10-11 04:26:23 -0700

    sii

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - This calculates and prints the fibonacci numbers.
+ * main - This calculates and prints the fibonacci nums.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
+int i;
 int n = 50;
 long fib[50];
 
 fib[0] = 1;
 fib[1] = 2;
 
-int i;
 for (i = 2; i < n; i++)
 {
 	fib[i] = fib[i - 1] + fib[i - 2];
