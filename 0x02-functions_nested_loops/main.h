c1d70fce2c4fb5a5a62015353fcf7fd49d3b71aa
Mubarak <yolkadeolu@gmail.com>
2023-10-10 17:13:14 -0700

    2ndtest

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 /* function that checks for a lowercase letter */
 int _islower(int c);
+/* function that checks for alphabet order */
+int _isalpha(int c);
 
 #endif /* MAIN_H */
