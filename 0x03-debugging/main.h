f931ee8ad4cb40c37634de9f2df3bb187404b44b
Mubarak <yolkadeolu@gmail.com>
2023-10-14 06:23:57 -0700

    now

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /* This function prints the largest of three integers */
 int largest_number(int a, int b, int c);
 /* Prints how many days are left in a year */
 void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */
