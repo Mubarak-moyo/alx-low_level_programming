51fb4fb48630278db98c207eee48a2bd10b94f84
Mubarak <yolkadeolu@gmail.com>
2023-10-16 15:07:05 -0700

    test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function updates the value a pointer points to.
+ * @n: This is the pointer.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This is the Prototype for _putchar */
+int _putchar(char);
+/* function that checks for uppercase letter */
+int _isupper(int c);
+/* function that checks for a digit */
+int _isdigit(int c);
+/* function that multiplies two integers */
+int mul(int a, int b);
+/* function that prints numbers */
+void print_numbers(void);
+/* function that prints the numbers from 0 to 9 */
+void print_most_numbers(void);
+/* function that prints 10 times the number */
+void more_numbers(void);
+/* function that draws a straight line */
+void print_line(int n);
+/* function that draws a diagonal line */
+void print_diagonal(int n);
+/* function that prints a square */
+void print_square(int size);
+/* function that prints a triangle */
+void print_triangle(int size);
+/* function that prints an integer */
+void print_number(int n);
+/* function that takes a pointer to an int as a parameter and updates its value */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
