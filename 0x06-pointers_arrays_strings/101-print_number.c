a70ce042fe534b723d6192ebfd85238b022873a2
Mubarak <yolkadeolu@gmail.com>
2023-10-19 12:56:35 -0700

    pp

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_number - this function prints an integer.
@@ -9,7 +10,15 @@ void print_number(int n)
 	if (n < 0)
 	{
 	_putchar('-');
+	if (n == INT_MIN)
+	{
+	_putchar('2');
+	n = 147483648;
+	}
+	else
+	{
 	n = -n;
+	}
 	}
 
 	if (n / 10 != 0)
