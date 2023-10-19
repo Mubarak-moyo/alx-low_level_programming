c01c22424692ccf83b18af1dc8d7db42dbb6e25e
Mubarak <yolkadeolu@gmail.com>
2023-10-19 14:03:45 -0700

    ww

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,19 +4,24 @@
  *
  * Return: A pointer to the mainpulated string.
  */
-char *leet(char *str) {
-	    int i, j;
-	        char leet_map[] = "aAeEoOtTlL";
-		    char leet_replace[] = "44337711";
+char *leet(char *str)
+{
+	    char *s = str;
+	        char *leet_replace = "aAeEoOtTlL";
+		    char *leet_encoding = "4433007711";
+		        int i, j;
 
-		        for (i = 0; str[i] != '\0'; i++) {
-				        for (j = 0; leet_map[j] != '\0'; j++) {
-						            if (str[i] == leet_map[j]) {
-								                    str[i] = leet_replace[j];
-										                    break;
-												                }
-							            }
-					    }
+			    for (i = 0; s[i] != '\0'; i++)
+				        {
+						        for (j = 0; leet_replace[j] != '\0'; j++)
+								        {
+										            if (s[i] == leet_replace[j])
+												                {
+															                s[i] = leet_encoding[j];
+																	                break;
+																			            }
+											            }
+							    }
 
-			    return (str);
+			        return str;
 }
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -3,15 +3,11 @@
 
 int main(void)
 {
-	    print_number(98);
-	        _putchar('\n');
-		    print_number(402);
-		        _putchar('\n');
-			    print_number(1024);
-			        _putchar('\n');
-				    print_number(0);
-				        _putchar('\n');
-					    print_number(-98);
-					        _putchar('\n');
-						    return (0);
+	    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	        char *p;
+
+		    p = leet(s);
+		        printf("%s", p);
+			    printf("%s", s);
+			        return (0);
 }
