66a1b601881e46639db3706269abae1020acb6a9
Mubarak <yolkadeolu@gmail.com>
2023-10-19 10:14:41 -0700

    tt

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -65,5 +65,7 @@ char *cap_string(char *);
 char *leet(char *);char *leet(char *);
 /* function that encodes a string using rot13 */
 char *rot13(char *);
+/* function that prints an integer */
+void print_number(int n);
 
 #endif /* MAIN_H */
