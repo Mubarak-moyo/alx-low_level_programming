aac666bf377646607096c8dfb7843b54f0d6099a
Mubarak <yolkadeolu@gmail.com>
2023-10-19 14:09:06 -0700

    ww

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,22 @@
  */
 char *leet(char *str)
 {
-	    char *s = str;
-	        char *leet_replace = "aAeEoOtTlL";
-		    char *leet_encoding = "4433007711";
-		        int i, j;
+	char *s = str;
+	char *leet_replace = "aAeEoOtTlL";
+	char *leet_encoding = "4433007711";
+	int i, j;
 
-			    for (i = 0; s[i] != '\0'; i++)
-				        {
-						        for (j = 0; leet_replace[j] != '\0'; j++)
-								        {
-										            if (s[i] == leet_replace[j])
-												                {
-															                s[i] = leet_encoding[j];
-																	                break;
-																			            }
-											            }
-							    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; leet_replace[j] != '\0'; j++)
+	{
+	if (s[i] == leet_replace[j])
+	{
+	s[i] = leet_encoding[j];
+	break;
+	}
+	}
+	}
 
-			        return str;
+	return (str);
 }
