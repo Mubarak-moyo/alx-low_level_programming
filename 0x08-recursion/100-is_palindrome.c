9768c90c470f95b19e9fa8f2ba87966ba7fd93fc
Mubarak <yolkadeolu@gmail.com>
2023-10-25 15:22:31 -0700

    gg

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
-int is_palindrome_recursive(char *s, int start, int end) {
+#include "main.h"
+
+int check_palindrome(char *s, int start, int end) {
 	    if (start >= end) {
 		            return 1;
 			        }
-
 	        if (s[start] != s[end]) {
 			        return 0;
 				    }
+		    return check_palindrome(s, start + 1, end - 1);
+}
 
-		    return is_palindrome_recursive(s, start + 1, end - 1);
+int get_string_length(char *s) {
+	    if (*s == '\0') {
+		            return 0;
+			        }
+	        return 1 + get_string_length(s + 1);
 }
 
 int is_palindrome(char *s) {
-	    int length = 0;
-	        while (s[length] != '\0') {
-			        length++;
+	    int length = get_string_length(s);
+	        if (length <= 1) {
+			        return 1;
 				    }
-
-		    if (length <= 1) {
-			            return 1;
-				        }
-
-		        return is_palindrome_recursive(s, 0, length - 1);
+		    return check_palindrome(s, 0, length - 1);
 }
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,7 +16,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 /* function that returns 1 if the input integer is a prime number, otherwise return 0. */
 int is_prime_number(int n);
-/* function that returns 1 if a string is a palindrome and 0 if not. */
+/* function that returns 1 if a string is a palindrome and 0 if not */
 int is_palindrome(char *s);
 
 #endif /* MAIN_H */
