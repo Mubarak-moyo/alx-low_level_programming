6198e88a90e1390d9e242dd3ec6d638c3aee3b39
Mubarak <yolkadeolu@gmail.com>
2023-10-25 14:35:36 -0700

    pp

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 int is_prime_number(int n) {
-	int i;
-
-	if (n < 2) {
-		      return 0;
-		        }
-
-	    for (i = 2; i * i <= n; i++) {
-		        if (n % i == 0) {
-				      return 0;
-				          }
-			  }
-
-	      return 1;
+	    if (n <= 1) {
+		            return 0;
+			        }
+	        if (n <= 3) {
+			        return 1;
+				    }
+		    if (n % 2 == 0 || n % 3 == 0) {
+			            return 0;
+				        }
+		        return is_prime_recursive(n, 5);
 }
+
