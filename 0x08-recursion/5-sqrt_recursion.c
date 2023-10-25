88cdc7b397b0b12fd93fd86ffc9000f9329cfe6d
Mubarak <yolkadeolu@gmail.com>
2023-10-25 15:00:48 -0700

    ww

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,29 @@
 #include "main.h"
 
+int sqrt_helper(int n, int low, int high) {
+	    if (low > high) {
+		            return -1;
+			        }
+
+	        int mid = (low + high) / 2;
+		    int square = mid * mid;
+
+		        if (square == n) {
+				        return mid;
+					    } else if (square < n) {
+						            return sqrt_helper(n, mid + 1, high);
+							        } else {
+									        return sqrt_helper(n, low, mid - 1);
+										    }
+}
+
 int _sqrt_recursion(int n) {
-	  if (n < 0) {
-		      return -1;
-		        } else if (n == 0 || n == 1) {
-				    return n;
-				      } else {
-					          int guess = n / 2;
-						      int square = guess * guess;
-						          if (square == n) {
-								        return guess;
-									    } else if (square > n) {
-										          return _sqrt_recursion(n - 1);
-											      } else {
-												            return _sqrt_recursion(n - square);
-													        }
-							    }
+	    if (n < 0) {
+		            return -1;
+			        }
+	        if (n == 0 || n == 1) {
+			        return n;
+				    }
+
+		    return sqrt_helper(n, 0, n);
 }
