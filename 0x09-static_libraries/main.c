89600862266d34b48fd3a514c4f5076ecda252a4
Mubarak <yolkadeolu@gmail.com>
2023-10-27 11:55:12 -0700

    First commit

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str = "\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick";
+	int i = 0;
+        while (str[i] != '\0') {
+		        _putchar(str[i]);
+			        i++;
+				    }
+	    return (0);
+}
